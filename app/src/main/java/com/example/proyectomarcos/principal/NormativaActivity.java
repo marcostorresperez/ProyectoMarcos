75b4b3ab1218a9ec0926fee740cb30914dba6175
marcostorresperez <torresperezmarcos93@gmail.com>
2021-05-29 17:58:16 +0200

    montado normativa

--- app/src/main/java/com/example/proyectomarcos/principal/NormativaActivity.java
+++ app/src/main/java/com/example/proyectomarcos/principal/NormativaActivity.java
@@ -6,28 +6,45 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
 
 import com.example.proyectomarcos.R;
+import com.example.proyectomarcos.extras.RecibirPDFStream;
+import com.github.barteksc.pdfviewer.PDFView;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 
 public class NormativaActivity extends AppCompatActivity {
+    PDFView lectorPDF;
 
- /*   @Override
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normativa);
+        String url = "https://www.dropbox.com/s/1z9kgyiv57gph25/normativaCoto.pdf?dl=0";
+        WebView view = (WebView) this.findViewById(R.id.wv);
+        view.getSettings().setJavaScriptEnabled(true);
 
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("C:\\Users\\Usuario\\AndroidStudioProjects\\ProyectoMarcos\\app\\src\\main\\res\\extras\\normativa.pdf"));
-        intent.setType("application/pdf");
-        PackageManager pm = getPackageManager();
-        List<ResolveInfo> activities = pm.queryIntentActivities(intent, 0);
-        if (activities.size() > 0) {
-            startActivity(intent);
-        } else { // Do something else here. Maybe pop up a Dialog or Toast }
-        }
+        view.loadUrl(url);
+
+
+//        lectorPDF = findViewById(R.id.pdfLector);
+//        String urlPDF="https://www.dropbox.com/s/1z9kgyiv57gph25/normativaCoto.pdf?dl=0";
+
     }
 
-  */
 }
